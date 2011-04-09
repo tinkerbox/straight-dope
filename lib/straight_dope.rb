f813cfc1feb9fcf3322c33927639165868d253ff
Jaryl Sim <jaryl@tinkerbox.com.sg>
2011-04-09 18:05:15 +0800

    Added a yfrog adapter.

--- lib/straight_dope.rb
+++ lib/straight_dope.rb
@@ -1,3 +1,43 @@
 module StraightDope
-  # Your code goes here...
+  
+  class YfrogAdapter
+    def self.match?(url)
+      !(url =~ /yfrog\.com/).nil?
+    end
+    
+    def self.extract(url)
+      uri = URI.parse(url)
+      id = uri.path
+      "http://yfrog.com/#{uri.path}:medium"
+    end
+  end
+  
+  def self.extract_media(content)
+    urls = URI.extract content
+    media_urls = []
+    
+    urls.each do |u|
+      response = Net::HTTP.get_response(URI.parse(u))
+      u = response['location']
+      
+      limit = 3
+      
+      while limit > 0 && response.kind_of?(Net::HTTPRedirection)
+        response = Net::HTTP.get_response(URI.parse(u))
+        u = response['location']
+        limit -= 1
+      end
+      
+      adapters = [YfrogAdapter]
+      adapters.each do |adapter|
+        if adapter.match? u
+          media_urls << adapter.extract(u)
+          break
+        end
+      end
+    end
+    
+    media_urls
+  end
+  
 end
